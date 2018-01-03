e054665e14b5722aa05a0fd51d7c5a132a1c355f
Tom White <fredley@gmail.com>
2018-01-03 21:53:15 +0000

    Add middleware

---
+++ lez/middleware.py
@@ -0,0 +1,13 @@
+from django.shortcuts import redirect
+from django.core.exceptions import MiddlewareNotUsed
+
+class ForceSecureMiddleware(object):
+"""Middleware that redirects http to https traffic."""
+
+def __init__(self):
+    if settings.DEBUG or not SITE_DOMAIN:
+        raise MiddlewareNotUsed
+
+def process_request(self, request):
+    if not request.is_secure():
+        return redirect("https://" + request.get_host() + request.get_full_path())
