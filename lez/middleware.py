1844c2ddcc8fc86cc85f3cd7ed968f186aabb35b
Tom White <fredley@gmail.com>
2018-01-03 21:56:57 +0000

    Fix middleware

--- lez/middleware.py
+++ lez/middleware.py
@@ -1,13 +1,12 @@
 from django.shortcuts import redirect
+from django.conf import settings
 from django.core.exceptions import MiddlewareNotUsed
 
 class ForceSecureMiddleware(object):
   """Middleware that redirects http to https traffic."""
 
-  def __init__(self):
-    if settings.DEBUG:
-      raise MiddlewareNotUsed
-
   def process_request(self, request):
+    if settings.DEBUG:
+        return
     if not request.is_secure():
       return redirect("https://" + request.get_host() + request.get_full_path())
