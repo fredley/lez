50b8d36de3dfbc2647e233a2a087fba0b0680093
Tom White <fredley@gmail.com>
2018-01-03 21:55:08 +0000

    Fix syntax error

--- lez/middleware.py
+++ lez/middleware.py
@@ -2,12 +2,12 @@ from django.shortcuts import redirect
 from django.core.exceptions import MiddlewareNotUsed
 
 class ForceSecureMiddleware(object):
-"""Middleware that redirects http to https traffic."""
+  """Middleware that redirects http to https traffic."""
 
-def __init__(self):
+  def __init__(self):
     if settings.DEBUG:
-        raise MiddlewareNotUsed
+      raise MiddlewareNotUsed
 
-def process_request(self, request):
+  def process_request(self, request):
     if not request.is_secure():
-        return redirect("https://" + request.get_host() + request.get_full_path())
+      return redirect("https://" + request.get_host() + request.get_full_path())
